d1252e95d3dcc7ad989974c3ce3bfc13d4503f1e
doctor-01111010 <doctor-01111010@gmail.com>
2025-07-22 19:21:44 +0300

    reset --hard

--- js/site.js
+++ js/site.js
@@ -8,6 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
+// Active nav link
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav a");
 
